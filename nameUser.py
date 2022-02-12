0bbd4a4271789c7c3b49ebc7d9a379e3f83bf005
vdktm <twixelzz@gmail.com>
2022-02-12 15:22:31 +0300

    update nameUser

--- nameUser.py
+++ nameUser.py
@@ -5,12 +5,16 @@ newDict = {}
 
 print("Для выхода из программы, введите 'q' два раза")
 
-while not name == 'q':
+while True:
     name = input('Введите имя: ')
+    if name == 'q':
+        break
     tele = input('Введите номер телефона: ')
     userDict[name] = tele
 
 for name in userDict.keys():
-    if name[0] == 'А':
+    if name[0] == 'A':
         newDict[name] = userDict[name]
+
 print(newDict)
+
